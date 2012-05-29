91e0d68cdcda612f5db79f3453ae4c3f8d2f57be
Steve Pike <spike@maplecroft.com>
2012-05-29 14:15:14 +0100

    args for script

--- util.py
+++ util.py
@@ -23,3 +23,26 @@ def load_rasters_from_dir(dirname):
 #        os.system(cmd)
 
 
+def main():
+    parser = argparse.ArgumentParser(description='Prepare and load rasters')
+    parser.add_argument('--prepare', dest='prepare', action='store_true',
+                        default=False,
+                        help='Create the table')
+    parser.add_argument('--dirname', dest='dirname', action='store',
+                        default=False,
+                        help='Load rasters from dir <dirname>')
+
+    args = parser.parse_args()
+    if args.prepare:
+        prepare_table()
+    if args.dirname:
+        load_rasters_from_dir(args.dirname)
+    if not args.prepare and not args.dirname:
+        parser.print_help()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
