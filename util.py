6530facb30fda14401a5737d6e5091fe8a1329f2
Steve Pike <spike@maplecroft.com>
2012-05-29 14:39:43 +0100

    curiousness of preparation... need a raster named the same as the table?? odd.

--- util.py
+++ util.py
@@ -9,18 +9,21 @@ import conf
 def prepare_table():
     """Sets up the table in the database"""
     cmd = "raster2pgsql -p -I -F %s | psql %s" % (conf.TABLE, conf.DBNAME)
+    print cmd
     os.system(cmd)
 
 
 def load_rasters_from_dir(dirname):
     """Take all the tif files and load them."""
     for fn in os.listdir(dirname):
-        cmd = "raster2pgsql -a -M -F -t 64x64 %s %s | psql %s" % (
+        cmd = "touch %s; raster2pgsql -a -M -F -t 64x64 %s %s | psql %s; rm %s" % (
+            conf.TABLE,
             os.path.join(dirname, fn),
             conf.TABLE,
-            conf.DBNAME)
+            conf.DBNAME,
+            conf.TABLE)
         print cmd
-#        os.system(cmd)
+        os.system(cmd)
 
 
 def main():
